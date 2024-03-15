a967910b3e7608a724806c1da1819c9df1ff163d
Schledi777 <130638123+Schledi777@users.noreply.github.com>
2024-03-15 16:54:40 +0100

    Update Disco_v1.py

--- Programme/Disco_v1.py
+++ Programme/Disco_v1.py
@@ -1,14 +1,21 @@
 #used lib neopixel.py (renamed argbled_lib.py) => used class Neopixel (renamed class Argled)
 #I had to rename the lib and class cause I hab a Error finding the class Neopixel from lib neopixel.py, after I renamed it Code worked
 #March 2024 
-from machine import Pin
+from machine import Pin, I2C
 from argbled_lib import Argbled
+from sh1106 import SH1106_I2C
 import utime
 import random
 
-led_r = Pin(27, Pin.OUT)
-led_g = Pin(26, Pin.OUT)
-led_b = Pin(22, Pin.OUT)
+WIDTH = 128
+HEIGHT = 64          #Pixelgröße Bildschirm
+i2c = I2C(0, scl = Pin(5), sda = Pin(4), freq=400000)
+display = SH1106_I2C(WIDTH, HEIGHT, i2c)
+display.rotate(True)
+
+led_r = Pin(14, Pin.OUT)
+led_g = Pin(12, Pin.OUT)
+led_b = Pin(13, Pin.OUT)
 ledPi = Pin(25, Pin.OUT)
 numpix = 64
 np = Argbled(numpix, 0, 2, "RGB")
@@ -27,11 +34,16 @@ utime.sleep(delay)
 np.brightness(42)
 blank = (0,0,0)
 
+display.text("HTWK Leipzig",20,0)
+display.text("Prg: Disco.py",10,28)
+display.show()
+
 #clear common Kathode LED
 ledPi.value(1)
 led_r.value(0)
 led_g.value(0)
 led_b.value(0)
+
 while True:
     led_r.toggle()
     utime.sleep(1)
