f3fe0a4bcae62e29d2f0ac702c9cceaa429da87e
Schledi777 <130638123+Schledi777@users.noreply.github.com>
2024-03-16 15:53:59 +0100

    Update RGBtoggleswitch.py

--- Programme/RGBtoggleswitch.py
+++ Programme/RGBtoggleswitch.py
@@ -1,18 +1,25 @@
 #Code by Daniel S. & Michael E., last update March 2024
-from machine import Pin
+
+from machine import Pin, reset
 from time import sleep
-Led_g = 15
-Led_b = 14
-Led_r = 6
+import sys
+#variable for Pin of togglebutton & LED
+Led_g = 12
+Led_b = 13
+Led_r = 10
 butPin = 18
 butPinx = 19
 butPinxx = 20
+#Button for returning to Menu
+button_pin = Pin(9, Pin.IN, Pin.PULL_UP) 
+#Togglebutton & LED declaration
 mybut = Pin(butPin,Pin.IN,Pin.PULL_UP)
 myLed = Pin(Led_g,Pin.OUT)
 mybutx = Pin(butPinx,Pin.IN,Pin.PULL_UP)
 myLedx = Pin(Led_b,Pin.OUT)
 mybutxx = Pin(butPinxx,Pin.IN,Pin.PULL_UP)
 myLedxx = Pin(Led_r,Pin.OUT)
+#definition of the States
 butStateNOW = 1
 butStateOLD = 1
 butStateNOWx = 1
@@ -23,6 +30,23 @@ LEDState=False
 LEDStatex=False
 LEDStatexx=False
 
+
+#RETURN TO MENU
+debounce_time = 50
+
+def button_pressed(pin):
+    from main import main  # Importiere die main-Funktion aus main.py
+    time.sleep_ms(debounce_time)  # Wartezeit für den Knopf-Debouncing
+    if button_pin.value() == 0:
+        # Der Knopf wurde gedrückt
+        print("Knopf wurde gedrückt! Starte main.py.")
+        main()  # Starte die main-Funktion aus main.py
+        sys.exit("main.py")
+
+# Dem Knopf einen Interrupt-Handler hinzufügen
+button_pin.irq(trigger=Pin.IRQ_FALLING, handler=button_pressed)
+
+#infinity loop
 while True:
     butStateNOW=mybut.value()
     if butStateNOW == 1 and butStateOLD == 0:
@@ -44,4 +68,3 @@ while True:
         myLedxx.value(LEDStatexx)
     butStateOLDxx = butStateNOWxx
     sleep(0.1)
-    
