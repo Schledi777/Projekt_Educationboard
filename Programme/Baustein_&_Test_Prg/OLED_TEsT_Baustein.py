26ada314e1eca65e5dd9257617367435ba9b08da
Schledi777 <130638123+Schledi777@users.noreply.github.com>
2024-03-17 08:55:07 +0100

    Update OLED_TEsT_Baustein.py

--- Programme/Baustein_&_Test_Prg/OLED_TEsT_Baustein.py
+++ Programme/Baustein_&_Test_Prg/OLED_TEsT_Baustein.py
@@ -1,13 +1,14 @@
 from machine import I2C, Pin
 from sh1106 import SH1106_I2C
 import time
-
+#Definition Breite und Höhe
 WIDTH = 128
 HEIGHT = 64
 #Display Pin Belegung
 i2c = I2C(0, scl = Pin(5), sda = Pin(4), freq=400000)
-
+#Definition der Variable für das Display Name ist frei wählbar
 display = SH1106_I2C(WIDTH, HEIGHT, i2c)
+#Dreht die Anzeige um 180 Grad
 display.rotate(True)
 
 while True:
